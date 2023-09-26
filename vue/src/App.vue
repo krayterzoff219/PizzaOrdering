d5a5b9905dddf8d47778cdb85acd5a6c0d5851bc
Dean McNeil <deanolmcneil@gmail.com>
2023-09-26 12:23:06 -0400

    Add logos, css reset, and nav bar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
-    <router-view />
-  </div>
+	<div id="app">
+		<nav-bar></nav-bar>
+		<!-- <div id="nav">
+			<router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+			<router-link
+				v-bind:to="{ name: 'logout' }"
+				v-if="$store.state.token != ''"
+				>Logout</router-link
+			>
+		</div> -->
+		<router-view />
+	</div>
 </template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+	components: { NavBar },
+};
+</script>
+
+<style>
+@import url(css/styles.css);
+
+body,
+#app {
+	background-color: var(--dark-color);
+}
+
+#app {
+	margin: 0;
+	padding-top: 5px;
+	height: 100%;
+	width: 100%;
+}
+
+#login {
+	background-color: var(--white-color);
+}
+</style>
