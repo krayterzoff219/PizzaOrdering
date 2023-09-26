b08bc0ee8b33b3503040df040c08be8f8c2f6719
Dean McNeil <deanolmcneil@gmail.com>
2023-09-26 15:49:15 -0400

    Finish adding user input component to employee register view

--- vue/src/components/UserInput.vue
+++ vue/src/components/UserInput.vue
@@ -1,21 +1,26 @@
 <template>
-    <div>
-        <label :for="inputId">{{label}}</label>
-        <input type="text" :id="inputId"/>
-    </div>
+	<div class="form-input-group">
+		<label :for="inputId">{{ label }}</label>
+		<input
+			:type="inputType"
+			:id="inputId"
+			v-model="localValue" />
+	</div>
 </template>
 
 <script>
-
-export default{
-
-    props:[
-        "label", "inputId",
-        "bindingElement",
-    ]
-
-   
-
-}
-
+export default {
+	props: ["label", "inputId", "inputType"],
+	data() {
+		return {
+			localValue: "",
+		};
+	},
+	watch: {
+		localValue(newValue) {
+			// Emit an input event to the parent to update the parent component's value
+			this.$emit("input", newValue);
+		},
+	},
+};
 </script>
