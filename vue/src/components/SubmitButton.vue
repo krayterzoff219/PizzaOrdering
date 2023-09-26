3e59cc57dd97a8c98c3e7e38e5b2af5256600c32
Shearson Lavergne <lavergne.shear@gmail.com>
2023-09-26 14:25:50 -0500

    button

---
+++ vue/src/components/SubmitButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button type="submit">{{buttonText}}</button>
+    </div>
+</template>
+
+<script>
+
+export default{
+    props: [
+        "buttonText"
+    ]
+}
+</script>
