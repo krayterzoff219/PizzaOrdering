2cf6478d9d46f592bda93c59a06705a3e3f6f295
Shearson Lavergne <lavergne.shear@gmail.com>
2023-10-02 14:05:48 -0500

    pendingOrders

---
+++ vue/src/components/OrderItem.vue
@@ -0,0 +1,20 @@
+<template>
+    
+        <div class="pendingList" >
+
+            
+
+		</div>
+
+   
+
+
+</template>
+
+ <script>
+
+        export default{
+
+        }
+
+    </script>
--- vue/src/views/PendingOrders.vue
+++ vue/src/views/PendingOrders.vue
@@ -2,17 +2,43 @@
 	<section
 		id="pending-orders"
 		class="employee-section">
-		Pending Orders
+		<horizontal-hero></horizontal-hero>
+		<div class="pending-order-wrapper">
+			<h1>Pending Orders</h1>
+			<order-item
+			v-for="order of pendingOrders"
+			:key="order.id"
+			:order="order"
+			/>
+		</div>
+		
 	</section>
 </template>
 
 <script>
-export default {
+import HorizontalHero from "../components/HorizontalHero.vue";
+import OrderService from '../services/OrderService';
+import OrderItem from '../components/OrderItem';
+
+
+export default { 
+
+  components: { HorizontalHero, OrderItem},
+
 	name: "pending-orders",
+	
 	beforeCreate() {
 		if (Object.keys(this.$store.state.cart).length) {
 			this.$store.commit("CLEAR_CART");
 		}
+		OrderService.getPendingOrders()
 	},
+	destroyed(){
+		this.$store.commit("LOAD_PENDING_ORDERS")
+	}
+
+
+
+
 };
 </script>
