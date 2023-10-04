fb420ab3274271eddf556fb50d15be9eb145d0df
Dean McNeil <deanolmcneil@gmail.com>
2023-10-04 15:16:48 -0400

    add name to my account info; populate checkout view with user info if there is a user signed in

--- vue/src/views/MyAccount.vue
+++ vue/src/views/MyAccount.vue
@@ -8,19 +8,15 @@
 			<an-alert
 				v-if="alertText"
 				:message="alertText" />
+
 			<user-input
-				label="Address"
-				inputId="my-account-address-input"
+				label="Name"
+				inputId="my-account-name-input"
 				inputType="text"
-				v-model="address"
-				:defaultValue="address"
+				v-model="name"
+				:defaultValue="name"
 				:isRequired="true" />
-			<!-- <user-input
-				label="Credit Card Number"
-				inputId="my-account-card-number-input"
-				inputType="number"
-				v-model="cardNumber"
-				:isRequired="true" /> -->
+
 			<user-input
 				label="Email"
 				inputId="my-account-email-input"
@@ -28,6 +24,7 @@
 				v-model="email"
 				:defaultValue="email"
 				:isRequired="true" />
+
 			<user-input
 				label="Phone Number"
 				inputId="my-account-phone-input"
@@ -35,6 +32,42 @@
 				v-model="phone"
 				:defaultValue="phone"
 				:isRequired="true" />
+
+			<user-input
+				label="Address"
+				inputId="my-account-address-input"
+				inputType="text"
+				v-model="address"
+				:defaultValue="address"
+				:isRequired="true" />
+			<user-input
+				label="City"
+				inputId="my-account-city-input"
+				inputType="text"
+				v-model="city"
+				:defaultValue="city"
+				:isRequired="true" />
+			<user-input
+				label="State"
+				inputId="my-account-state-input"
+				inputType="text"
+				v-model="state"
+				:defaultValue="state"
+				:isRequired="true" />
+			<user-input
+				label="ZIP Code"
+				inputId="my-account-zip-input"
+				inputType="number"
+				v-model="zipCode"
+				:defaultValue="zipCode"
+				:isRequired="true" />
+			<!-- <user-input
+				label="Credit Card Number"
+				inputId="my-account-card-number-input"
+				inputType="number"
+				v-model="cardNumber"
+				:isRequired="true" /> -->
+
 			<small-button
 				buttonText="Save Changes"
 				buttonType="submit" />
@@ -53,30 +86,55 @@ export default {
 	data() {
 		return {
 			address: null,
+			city: "",
+			state: "",
+			zipCode: "",
 			cardNumber: "",
 			dataId: -1,
 			email: null,
 			phone: 0,
 			userId: -1,
 			alertText: "",
+			name: "",
 		};
 	},
 	created() {
-		const { address, cardNumber, dataId, email, phone, userId } =
+		const { address, cardNumber, dataId, email, phone, userId, name } =
 			this.$store.state.user.userData;
-		this.address = address;
+		// this.address = address;
+		if (address) {
+			const addressArray = address.split("|||");
+			this.address = addressArray[0];
+			this.city = addressArray[1];
+			this.state = addressArray[2];
+			this.zipCode = addressArray[3];
+		}
 		this.cardNumber = cardNumber;
 		this.dataId = dataId;
 		this.email = email;
 		this.phone = phone;
 		this.userId = userId;
+		this.name = name;
 	},
 	methods: {
 		updateUserInfo() {
-			const { address, cardNumber, dataId, email, phone, userId } = this;
-			const userData = { address, cardNumber, dataId, email, phone, userId };
+			const {
+				address,
+				cardNumber,
+				dataId,
+				email,
+				phone,
+				userId,
+				city,
+				state,
+				zipCode,
+				name,
+			} = this;
+			const userData = { cardNumber, dataId, email, phone, userId, name };
+			userData.address = [address, city, state, zipCode].join("|||");
 			userDataService.update(userData).then((res) => {
 				if (res.status === 200) {
+					this.$store.commit("UPDATE_USER", userData);
 					this.$router.push({ name: "home" });
 				}
 			});
