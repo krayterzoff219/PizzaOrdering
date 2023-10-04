dac4a9ed0d9e9e16b8bffd831f759be0e40d7f56
Shearson Lavergne <lavergne.shear@gmail.com>
2023-10-04 10:44:43 -0500

    orderView styles

--- vue/src/views/EmployeeViewOrderPage.vue
+++ vue/src/views/EmployeeViewOrderPage.vue
@@ -8,25 +8,29 @@
             <horizontal-hero></horizontal-hero>
             <h2>Order View</h2>
 
-            <p>Order status: {{currentOrder.status.charAt(0).toUpperCase() + currentOrder.status.slice(1)}}</p>
-            <br/>
-            <p>{{currentOrder.isDelivery ? "Delivery" : "Pick-up"}}</p>
-            <br/>
-            <p>Customer phone number: {{currentOrder.phone}}</p>
-            <br/>
-            <p>Customer address: {{currentOrder.address}}</p>
-            <br/>
-            <view-details-item
-            id="details-section"
-            v-for="Item of currentOrder.menuItems"
-            :key="Item.id"
-            :menuItem = "Item"
-            />
-            
+            <div class="information-section">
+                <p>Order status: {{currentOrder.status.charAt(0).toUpperCase() + currentOrder.status.slice(1)}}</p>
+                <br/>
+                <p>{{currentOrder.isDelivery ? "Delivery" : "Pick-up"}}</p>
+                <br/>
+                <p>Customer phone number: {{currentOrder.phone}}</p>
+                <br/>
+                <p>Customer address: {{currentOrder.address}}</p>
+                
+                <hr/>
+                
 
+                <view-details-item
+                id="details-section"
+                v-for="Item of currentOrder.menuItems"
+                :key="Item.id"
+                :menuItem = "Item"
+                />
+            </div>
 
-            
-       </div>
+           
+               
+        </div>
 
     </section>
 
@@ -80,9 +84,26 @@ export default{
 
 <style scoped>
 
+h2{
+    text-decoration: underline;
+    padding: 20px;
+}
 
-#details-section{
+.information-section{
+    padding-left: 30px;
+    padding-right: 30px;
     
 }
+
+hr{
+    width: 100%;
+    height: 3px;
+    
+}
+
+#details-section {
+    border-bottom: black solid 1px;
+}
+
 
 </style>
