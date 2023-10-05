78dd4fcc149c4de84f68db2b7d4eae3c373a6d59
Dean McNeil <deanolmcneil@gmail.com>
2023-10-05 15:44:35 -0400

    Update order demo data

--- java/database/testOrders.sql
+++ java/database/testOrders.sql
@@ -7,18 +7,18 @@ INSERT INTO user_data (user_id) VALUES (3);
 INSERT INTO user_data (user_id, name, email, address, phone, credit_card) VALUES (4, 'test', 'test@email', 'streetaddress|||city|||state|||12345', '1234567890', '123456789');
 
 
-INSERT INTO user_data (name, email, address, phone, credit_card) VALUES ('Richard', 'richard@email', '4456 somewhere|||columbus|||Ohio|||12345', '1234567890', '123456789');
+INSERT INTO user_data (name, email, address, phone, credit_card) VALUES ('Richard', 'richard@example.com', '1234 Somewhere Dr.|||Columbus|||Ohio|||12345', '1112223344', '123456789');
 INSERT INTO orders (status, data_id, delivery, subtotal, tax) VALUES ('pending', 5, true, 56.50, 4.52);
 INSERT INTO orders_to_menu_items (order_id, item_id, quantity) VALUES (1, 1, 2);
 INSERT INTO orders_to_menu_items (order_id, item_id, quantity) VALUES (1, 2, 1);
 INSERT INTO orders_to_menu_items (order_id, item_id, quantity) VALUES (1, 3, 1);
 
-INSERT INTO user_data (name, email, address, phone, credit_card) VALUES ('Margaret', 'margaret@email', '4456 somewhere|||columbus|||Ohio|||12345', '1234567890', '123456789');
-INSERT INTO orders (status, data_id, delivery, subtotal, tax) VALUES ('pending', 6, true, 29.50, 2.36);
+INSERT INTO user_data (name, email, address, phone, credit_card) VALUES ('Margaret', 'margaret@example.com', '2345 Anywhere Rd.|||Columbus|||Ohio|||12345', '2223334455', '123456789');
+INSERT INTO orders (status, data_id, delivery, subtotal, tax) VALUES ('pending', 6, false, 29.50, 2.36);
 INSERT INTO orders_to_menu_items (order_id, item_id, quantity) VALUES (2, 5, 2);
 INSERT INTO orders_to_menu_items (order_id, item_id, quantity) VALUES (2, 4, 1);
 
-INSERT INTO user_data (name, email, address, phone, credit_card) VALUES ('Daniel', 'daniel@email', '4456 somewhere|||columbus|||Ohio|||12345', '1234567890', '123456789');
+INSERT INTO user_data (name, email, address, phone, credit_card) VALUES ('Daniel', 'daniel@example.com', '3456 Nowhere Dr.|||Columbus|||Ohio|||12345', '3334445566', '123456789');
 INSERT INTO orders (status, data_id, delivery, subtotal, tax) VALUES ('pending', 7, true, 48.75, 3.90);
 INSERT INTO orders_to_menu_items (order_id, item_id, quantity) VALUES (3, 5, 1);
 INSERT INTO orders_to_menu_items (order_id, item_id, quantity) VALUES (3, 4, 1);
